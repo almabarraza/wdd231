33a83f8c02ddd918fee9d26075048349fb96cc89
almabarraza <abarrazaraygoza@byupathway.edu>
2025-05-12 20:25:26 -0700

    week 1 finished

---
+++ scripts/navigation.js
@@ -0,0 +1,9 @@
+
+/*This section create an event fot the hamburger menu in mobile view*/
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
